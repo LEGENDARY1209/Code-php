86f7e77b0b45ea7307850e83a3dcf520d3f51bc7
LEGENDARY1209 <130669909+LEGENDARY1209@users.noreply.github.com>
2023-09-22 09:13:06 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+
+        <?php 
+
+
+            //phpinfo();
+    
+            $h =date ("h");
+
+            if ($h >= 0 && $h < 12){
+                echo "<p>"  .$h. "hs. Bom dia à todos!</p>";
+            } else if ($h >= 12 && $h < 18 ) {  
+                echo "<p>"  .$h. "hs. Boa tarde à todos!</p>";
+            } else {
+                echo "<p>"  .$h. "hs. Boa noite à todos!</p>"; 
+            }
+        ?>
+    </body>
+</html>
